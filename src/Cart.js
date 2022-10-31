2cbb6524c9ad03bd6dd62fdcf898aa0ac61fd4fb
VIKAS KUMAR <kumar.vikas593@gmail.com>
2022-10-31 18:19:27 +0530

    cart

--- src/Cart.js
+++ src/Cart.js
@@ -4,13 +4,11 @@ import React from 'react';
 
 class Cart extends React.Component {
   render () {
-    const arr = [1, 2, 3, 4, 5];
+   
     return (
-      <div className="cart">
-        { arr.map((item) => {
-          return item + 5
-        }) }
-      </div>
+      <div> cart </div> 
+        
+      
     );
   }
 }
