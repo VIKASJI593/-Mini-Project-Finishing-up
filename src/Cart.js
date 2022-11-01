4843c37bd6fe3c454c05e70f89612de5348a293e
VIKAS <kumar.vikas593@gmail.com>
2022-11-02 00:10:02 +0530

    props test

--- src/Cart.js
+++ src/Cart.js
@@ -3,13 +3,57 @@ import React from 'react';
 import CartItem from './CartItem';
 
 class Cart extends React.Component {
+
+  constructor () {
+    super();
+    this.state = {
+      products: [
+        {
+          price: 99,
+          title: 'Watch',
+          qty: 1,
+          img: '',
+          id: 1
+        },
+        {
+          price: 999,
+          title: 'Mobile Phone',
+          qty: 10,
+          img: '',
+          id: 2
+        },
+        {
+          price: 999,
+          title: 'Laptop',
+          qty: 4,
+          img: '',
+          id: 3
+        }
+      ]
+    }
+  }
   render () {
+  const { products } = this.state;
     return (
-    <div className="cart">
-    <CartItem qty = {1} price = {99} title = {"watch"} img = {''}/>
-    </div>
+      <div className="cart">
+        {products.map((product) => {
+          return (
+            <CartItem
+              product={product}
+              key={product.id}
+              func = { () => console.log('sdsd')}
+            isloggedin ={false}
+            jsx = {<h1>test</h1>}
+              comp = {<CartItem/>}
+                
+              
+            />
+          )
+         })}
+      </div>
     );
   }
 }
+
 
 export default Cart;
--- src/CartItem.js
+++ src/CartItem.js
@@ -3,16 +3,7 @@
 import React from 'react';
 
 class CartItem extends React.Component {
-  constructor () {
-    super();
-    this.state = {
-      price: 999,
-      title: 'Mobile Phone',
-      qty: 1,
-      img: ''
-    }
-    
-  }
+  
   increaseQuantity = () => {
    
     this.setState((prevState) => {
@@ -37,9 +28,12 @@ class CartItem extends React.Component {
   }
   render () {
     console.log('this.props', this.props);
-    const { price, title, qty } = this.props;
+    const { price, title, qty } = this.props.product;
     return (
       <div className="cart-item">
+
+      {this.props.jsx}
+
       <div className="left-block">
       <img style={styles.image} alt = ""  
          src=" "/> 
